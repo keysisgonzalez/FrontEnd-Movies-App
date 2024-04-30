b5409111ec8c8ccc48a90f939d0a7cfb08167c2d
Keysis Gonzalez <keysis917@gmail.com>
2024-04-30 16:19:50 -0400

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Movies from "./components/movies/Movies";
+import debug from "sabio-debug";
+
+const _logger = debug.extend("App");
+function App() {
+  const [currentUser] = useState({
+    firstName: "",
+    isLoggedIn: false,
+    lastName: "",
+  });
+
+  function submitBtnCLiked() {
+    console.log("Clicking");
+  }
+
+  _logger("Something important somewhere in a function within your component");
+
+  return (
+    <React.Fragment>
+      <SiteNav user={currentUser}></SiteNav>
+      {/*Allows to change the url */}
+
+      <div className="container ">
+        <Routes>
+          <Route path="/" element={<Home user={currentUser} />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
