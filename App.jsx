e2cc21b05db45f75c22d322a0ac4f6c5ad72eb85
Keysis Gonzalez <keysis917@gmail.com>
2024-10-30 14:29:42 -0400

    -

--- App.jsx
+++ App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <React.Fragment>
       <SiteNav user={currentUser}></SiteNav>
-      {/*Allows to change the url*/}
+      {/*Allows tochange the url*/}
 
       <div className="container ">
         <Routes>
